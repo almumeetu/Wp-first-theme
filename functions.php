889fb07ab66eee0a5cca5391363d705d6c099b78
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-09-27 04:09:25 +0600

    study on wp

--- functions.php
+++ functions.php
@@ -6,3 +6,10 @@
 
 //Theme Title
 add_theme_support('title-tag');
+
+
+//Theme css and jQuery File calling
+function saikat_css_js_file_calling() {
+    wp_enqueue_style('wl-style',get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'saikat_css_js_file_calling');
