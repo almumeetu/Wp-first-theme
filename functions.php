6100c41d022f675ff5a9225b91bb51609972568d
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-09-27 04:21:06 +0600

    studing on WordPress Enqueue Scripts

--- functions.php
+++ functions.php
@@ -11,5 +11,9 @@ add_theme_support('title-tag');
 //Theme css and jQuery File calling
 function saikat_css_js_file_calling() {
     wp_enqueue_style('wl-style',get_stylesheet_uri());
+    wp_register_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_register_style( 'custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('custom');
 }
 add_action('wp_enqueue_scripts', 'saikat_css_js_file_calling');
