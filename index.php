eb2e978ad4b9d9f89d9e319cff9d55cc5f4c5c63
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-10-12 00:29:31 +0600

    study on wp slicing

--- index.php
+++ index.php
@@ -1,41 +1,9 @@
-<?php
-/* 
-* This template for displaying the header
-*/ 
-?>
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>" class="no-js">
-<head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<header id="header_area" class="<?php echo get_theme_mod('saikat_menu_position'); ?>">
+<?php 
+/*
+* The Header Template File
+*/
 
-    <div id="notice">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <p>Some Notification</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <a href="<?php echo home_url(); ?>"> 
-                    <img src="<?php echo get_theme_mod('saikat_logo'); ?>"alt="">
-                </a>
-            </div>
-            <div class="col-md-9">
-                <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
-            </div>
-        </div>
-    </div>
-</header>
+get_header(); ?>
 
 <Section id="body_area">
     <div class="container">
