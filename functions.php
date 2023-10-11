f356634b1e9b5afaca80479e9c5294258eb34322
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-10-11 14:10:20 +0600

    working on wp walker menu

--- functions.php
+++ functions.php
@@ -92,5 +92,16 @@ function saikat_customizar_register($wp_customize) {
 add_action('customize_register', 'saikat_customizar_register');
 
 
-//WordPress Menu Register
+// WordPress Menu Register
 register_nav_menu( 'main_menu', __('Main Menu', 'almumeetu') );
+
+
+// Walker Menu properties
+function saikat_nav_description($item_output, $item, $args){
+    if(!empty ($item->description)){
+        $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . 
+        $item->description . '<span>' . $args->link_after . '</a>', $item_output);
+    }
+    return $item_output;
+}
+add_filter('walker_nav_menu_start_el', 'saikat_nav_description', 10, 3);
